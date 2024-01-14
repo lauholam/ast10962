091cbaded96bae237b4eb140f5d851e86c085f79
lauholam <lauholam@gmail.com>
2024-01-14 11:01:22 +0800

    Add files via upload

---
+++ js/lab13.js
@@ -0,0 +1,16 @@
+"use strict;"
+/* AST10962 Lab12 Sample JS*/
+$(document).ready(function() {
+
+  // Lab 12 Task 3
+  // var $index = 0;
+  // var $pics = ['bg0.jpg', 'bg1.jpg', 'bg2.jpg', 'bg3.jpg'];
+  // $(document).keydown(function() {
+  //   var $key = event.which;
+	//
+  //   //check point 3: complete the logic with if-else or switch
+	//
+  //   var $imageUrl = 'images/' + $pics[$index];
+  //   $('body').css('background-image', 'url(' + $imageUrl + ')');
+  // });
+});
