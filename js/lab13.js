41e7a1444f72fbfcdebed53aa06d3377a258b9b6
lauholam <lauholam@gmail.com>
2024-01-14 11:06:05 +0800

    Update lab13.js

--- js/lab13.js
+++ js/lab13.js
@@ -1,16 +1,15 @@
 "use strict;"
-/* AST10962 Lab12 Sample JS*/
+/* AST10962 Lab13 Sample JS*/
 $(document).ready(function() {
 
-  // Lab 12 Task 3
-  // var $index = 0;
-  // var $pics = ['bg0.jpg', 'bg1.jpg', 'bg2.jpg', 'bg3.jpg'];
-  // $(document).keydown(function() {
-  //   var $key = event.which;
-	//
-  //   //check point 3: complete the logic with if-else or switch
-	//
-  //   var $imageUrl = 'images/' + $pics[$index];
-  //   $('body').css('background-image', 'url(' + $imageUrl + ')');
-  // });
+  var $index = 0;
+  var $pics = ['bg0.jpg', 'bg1.jpg', 'bg2.jpg', 'bg3.jpg'];
+  $(document).keydown(function() {
+    var $key = event.which;
+	
+    //check point 3: complete the logic with if-else or switch
+	
+    var $imageUrl = 'images/' + $pics[$index];
+    $('body').css('background-image', 'url(' + $imageUrl + ')');
+  });
 });
